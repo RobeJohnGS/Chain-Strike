992b3c4b834f8c28ce49d92394c6ad2fe6e9b3c6
RobeJohnGS <jroberts@student.neumont.edu>
2025-01-21 15:20:07 -0700

    Movement Broke again for no reason

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -32,13 +32,15 @@ public class InputManager : MonoBehaviour
         bikeGroundControlsActions.Trick1.performed += _ => trickControls.Trick1();
         bikeGroundControlsActions.Trick2.performed += _ => trickControls.Trick2();
 
-        cameraControlsActions.MouseX.performed += ctx => mouseInputManager.x = ctx.ReadValue<float>();
-        cameraControlsActions.MouseY.performed += ctx => mouseInputManager.y = ctx.ReadValue<float>();
+        
     }
 
     private void Update()
     {
-        bikeGroundControlsActions.Movement.performed += ctx => wasdInputManager = ctx.ReadValue<Vector2>();
+        //bikeGroundControlsActions.Movement.performed += ctx => wasdInputManager = ctx.ReadValue<Vector2>();
+        wasdInputManager = bikeGroundControlsActions.Movement.ReadValue<Vector2>();
+        cameraControlsActions.MouseX.performed += ctx => mouseInputManager.x = ctx.ReadValue<float>();
+        cameraControlsActions.MouseY.performed += ctx => mouseInputManager.y = ctx.ReadValue<float>();
         playerControls.RecieveInput(wasdInputManager);
         cameraControls.RecieveInput(mouseInputManager);
         //playerControls.ChangePlayerRotation(cameraControls.cameraFollow.transform.rotation.eulerAngles.y);
