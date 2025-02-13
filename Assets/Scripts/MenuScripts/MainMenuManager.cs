0fbb8acfaf0797ddbe7f60a4874fa4ad17c30da4
RobeJohnGS <jroberts@student.neumont.edu>
2025-02-13 16:03:38 -0700

    Main Menu

--- Assets/Scripts/MenuScripts/MainMenuManager.cs
+++ Assets/Scripts/MenuScripts/MainMenuManager.cs
@@ -1,16 +1,61 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Animations;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] CurrentMenu currentMenu;
+    [Header("UI Elements")]
+    /*
+     * UI Element 0: Click to continue text
+     * UI Element 1: Main menu buttons
+     */
+    [SerializeField] GameObject[] uiElements;
+    enum CurrentMenu
     {
-        
+        clickToStart,
+        menuSelection,
+        levelSelect,
+        customize,
+        settings
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        currentMenu = CurrentMenu.clickToStart;
+    }
+
+    private void Update()
+    {
+        switch (currentMenu)
+        {
+            case CurrentMenu.clickToStart:
+                if (Input.anyKeyDown)
+                {
+                    currentMenu = CurrentMenu.menuSelection;
+                    //Disable the click to continue text
+                    EnableUIElement(uiElements[0], false);
+                    //Enable the main buttons game object
+                    EnableUIElement(uiElements[1], true);
+                }
+                break;
+        }
+    }
+
+    public void SwitchMenu(int newMenu)
+    {
+        currentMenu = (CurrentMenu)newMenu;
+    }
+
+    private void EnableUIElement(GameObject uiOb, bool enable)
+    {
+        uiOb.SetActive(enable);
+    }
+
+    public void QuitGame()
+    {
+        EditorApplication.ExitPlaymode();
+        Application.Quit();
     }
 }
